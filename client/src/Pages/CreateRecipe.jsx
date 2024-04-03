eb8b1e15e29efae2b498cd48b014de9b32d5f064
rohit1396 <rohitgaikwad726@gmail.com>
2024-04-03 17:25:53 +0530

    add condition for create recipe

--- client/src/Pages/CreateRecipe.jsx
+++ client/src/Pages/CreateRecipe.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAuth } from "../context/context";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const CreateRecipe = () => {
   const [file, setFile] = useState(null);
@@ -13,7 +14,7 @@ const CreateRecipe = () => {
   });
   const navigate = useNavigate();
 
-  const { userData, token } = useAuth();
+  const { userLoggedIn, userData, token } = useAuth();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -88,68 +89,83 @@ const CreateRecipe = () => {
     }
   };
   return (
-    <div className="bg-slate-200 w-full min-h-screen mt-20 flex justify-center">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-slate-50 w-full md:max-w-lg h-auto flex flex-col m-10 justify-center items-center border border-slate-400 rounded-md"
-      >
-        <input
-          type="text"
-          placeholder="name"
-          className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
-          name="name"
-          value={recipe.name}
-          onChange={handleChange}
-        />
-        {recipe.ingredients.map((ingredient, idx) => (
+    <div
+      className={`${
+        userLoggedIn ? "bg-slate-200 min-h-screen" : "bg-slate-0 h-auto"
+      } w-full mt-20 flex justify-center`}
+    >
+      {userLoggedIn ? (
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-50 w-full md:max-w-lg h-auto flex flex-col m-10 justify-center items-center border border-slate-400 rounded-md"
+        >
           <input
-            key={idx}
             type="text"
-            placeholder="ingredients"
+            placeholder="name"
             className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
-            name="ingredients"
-            value={ingredient}
-            onChange={(e) => handleIngredientChange(e, idx)}
+            name="name"
+            value={recipe.name}
+            onChange={handleChange}
           />
-        ))}
-        <button
-          type="button"
-          className="w-4/5 h-8 p-1 m-1 bg-rose-800 rounded-lg text-slate-50 text-sm font-normal outline-none tracking-widest"
-          onClick={handleAddIngredient}
-        >
-          Add Ingredients
-        </button>
-        <input
-          type="file"
-          placeholder="image"
-          className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
-          name="imageUrl"
-          // value={recipe.imageUrl}
-          onChange={handleFileChange}
-        />
-        <input
-          type="text"
-          placeholder="instructions"
-          className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
-          name="instructions"
-          value={recipe.instructions}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="cooking time"
-          className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
-          name="cookingTime"
-          value={recipe.cookingTime}
-          onChange={handleChange}
-        />
-        <button
-          type="submit"
-          className="w-4/5 p-4 m-2 bg-rose-800 rounded-md text-slate-50 text-lg font-normal outline-none tracking-widest"
-        >
-          Create Recipe{" "}
-        </button>
-      </form>
+          {recipe.ingredients.map((ingredient, idx) => (
+            <input
+              key={idx}
+              type="text"
+              placeholder="ingredients"
+              className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
+              name="ingredients"
+              value={ingredient}
+              onChange={(e) => handleIngredientChange(e, idx)}
+            />
+          ))}
+          <button
+            type="button"
+            className="w-4/5 h-8 p-1 m-1 bg-rose-800 rounded-lg text-slate-50 text-sm font-normal outline-none tracking-widest"
+            onClick={handleAddIngredient}
+          >
+            Add Ingredients
+          </button>
+          <input
+            type="file"
+            placeholder="image"
+            className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
+            name="imageUrl"
+            // value={recipe.imageUrl}
+            onChange={handleFileChange}
+          />
+          <input
+            type="text"
+            placeholder="instructions"
+            className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
+            name="instructions"
+            value={recipe.instructions}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="cooking time"
+            className="w-4/5 p-4 m-2 border border-rose-800 rounded-lg text-gray-800 text-lg font-normal outline-none"
+            name="cookingTime"
+            value={recipe.cookingTime}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="w-4/5 p-4 m-2 bg-rose-800 rounded-md text-slate-50 text-lg font-normal outline-none tracking-widest"
+          >
+            Create Recipe{" "}
+          </button>
+        </form>
+      ) : (
+        <div className="flex flex-col justify-center items-center w-1/3 min-w-80 h-72">
+          <h3 className="text-slate-500 font-bold m-4">
+            Login To Create Your Recipes
+          </h3>
+          <button className="h-10 w-1/4 bg-rose-800 text-slate-50 text-lg tracking-widest rounded-md">
+            <Link to="/login">Login</Link>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
